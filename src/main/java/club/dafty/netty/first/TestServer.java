8e38d67467acfa0c1306032fb747ae258c562a18
lichengchao <lichengchao@aegis-data.cn>
2019-10-18 16:13:44 +0800

    Initial commit

---
+++ src/main/java/club/dafty/netty/first/TestServer.java
@@ -0,0 +1,34 @@
+package club.dafty.netty.first;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * @author 李成超
+ * @date 2019/10/18 14:39
+ * @description TODO
+ **/
+public class TestServer {
+    public static void main(String[] args) throws InterruptedException {
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            // 前一个接收到请求 发送给worker线程组
+            serverBootstrap.group(bossGroup,workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .childHandler(new TestServerInitializer());
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        }finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+
+    }
+}
---
+++ src/main/java/club/dafty/netty/first/TestServerInitializer.java
@@ -0,0 +1,22 @@
+package club.dafty.netty.first;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+
+/**
+ * @author 李成超
+ * @date 2019/10/18 14:43
+ * @description TODO
+ **/
+public class TestServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ChannelPipeline pipeline = ch.pipeline();
+
+        pipeline.addLast("httpServerCodec",new HttpServerCodec());
+        pipeline.addLast("testHttpServerHandler",new TestHttpServerHandler());
+    }
+}
